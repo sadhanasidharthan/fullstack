dc2ecff79759c943d43c1c33486f0693c47c6bc1
sadhanasidharthan <113581766+sadhanasidharthan@users.noreply.github.com>
2024-01-31 15:33:57 +0530

    Delete Day5 directory

--- Day5/AdminAccept.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AdminAccept = () => {
-  return (
-    <div>AdminAccept</div>
-  )
-}
-
-export default AdminAccept
--- Day5/AdminDetails.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AdminDetails = () => {
-  return (
-    <div>AdminDetails</div>
-  )
-}
-
-export default AdminDetails
